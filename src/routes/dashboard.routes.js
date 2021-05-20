6783efe36e626784b6d4b690482272cd98583abd
Fernando <fer.oliveeira@hotmail.com>
2021-05-20 19:41:35 -0300

    style: Top tab stylization

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import main from '../pages/Main';
 import maintenance from '../pages/Maintenances';
@@ -14,9 +15,35 @@ const DashboardRoutes = () => {
 
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator tabBarOptions={{ labelStyle: { fontSize: 15, color: 'black' } }}>
-          <Tab.Screen name="Principal" component={main} />
-          <Tab.Screen name="Manutenções" component={maintenance} />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'main') {
+
+                iconName = focused ? 'home-sharp' : 'home-outline';
+
+              } else if (route.name === 'maintenance') {
+
+                iconName = focused ? 'construct' : 'construct-outline';
+
+              }
+
+              return <Ionicons name={iconName} size={20} color={color} />
+            },
+          })}
+
+          tabBarOptions={{
+            showIcon: true,
+            tabStyle: { flexDirection: 'row', },
+            labelStyle: { fontSize: 15, },
+            activeTintColor: '#333',
+            inactiveTintColor: '#666'
+          }}>
+
+          <Tab.Screen name="main" component={main} options={{ title: 'Principal' }} />
+          <Tab.Screen name="maintenance" component={maintenance} options={{ title: 'Manutenções' }} />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
